5215b09bafb4768306caecd2a84e697b03ade434
Tania Papamikrou <tania.papamikrou@gmail.com>
2022-02-22 21:37:35 +0000

    gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+ #User.create(address_line_1: Faker::Address.street_address) do |address|
+  #address.city = Faker::Address.city
+  #address.postal_code = Faker::Address.zip
+  #address.country = Faker::Address.country
+# end
+
+
+#(1..20).each do |id|
+ #User.create(
+
+
+  #id: id,
+  #name: Faker::Name.name,
+  #last_name: Faker::Name.last_name,
+  #email: Faker::Internet.email,
+  #phone_number: Faker::PhoneNumber.cell_phone
+ #)
+#end
+
+#(1..20).each do |id|
+ #Place.create!(
+  #id: id,
+  #name: Faker::Restaurant.name,
+  #description: Faker::Restaurant.description,
+  #email: Faker::Internet.email,
+  #phone_number: Faker::PhoneNumber.phone_number,
+  #website: Faker::Internet.url
+ #)
+#end
