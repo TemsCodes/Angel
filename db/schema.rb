56b8924c96334ebc3a6b8405a82daed88a93aaf9
enora zine <enora.zine@gmail.com>
2022-02-24 21:47:48 +0100

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_213431) do
+ActiveRecord::Schema.define(version: 2022_02_24_193905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.date "date"
+    t.datetime "date"
     t.bigint "user_id", null: false
     t.bigint "place_id", null: false
     t.datetime "created_at", precision: 6, null: false
