1b8c40825e059665c457b3e74b69b33795614b5a
Renee Godyn <renitatje@hotmail.com>
2022-02-24 21:04:49 +0000

    created js file with reviews

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,8 +23,8 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
+import { review } from '../components/review';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  review();
 });
