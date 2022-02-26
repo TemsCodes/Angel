37f0c57ebb761ff6df20debae3c1cd4e9e565619
Tania Papamikrou <tania.papamikrou@gmail.com>
2022-02-26 14:26:24 +0000

    changes made with rubocop, added confrimation route and a message in the confirmation view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,44 +1,41 @@
 class BookingsController < ApplicationController
-    before_action :set_booking, only: [:show, :destroy]
+  before_action :set_booking, only: %i[show destroy]
 
-    def index
-      @bookings = Booking.all
-    end
+  def index
+    @bookings = Booking.all
+  end
 
-    def create
-      @booking = Booking.new(booking_params)
-      raise
-      @booking.user = current_user
-      if @booking.save
-        redirect_to profile_path
-      else
-        render :new
-      end
-    end
-
-    def new
-      @booking = Booking.new
-    end
-
-    def destroy
-      @booking.destroy
-      redirect_to experience_bookings_path
-    end
-
-    def show
-    end
-
-
-    private
-
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
-
-    def booking_params
-      params.require(:booking).permit(:user_id, :date, :place_id)
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to profile_path
+    else
+      render :new
     end
   end
 
+  def new
+    @booking = Booking.new
+  end
 
-  #add comment
+  def destroy
+    @booking.destroy
+    redirect_to experience_bookings_path
+  end
+
+  def show
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :date, :place_id)
+  end
+end
+
+# add comment
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
-
   def show
     @bookings = current_user.bookings
     @places = current_user.places
   end
 
   def new
-   @user = User.new
+    @user = User.new
   end
 
   def create
-   @user = User.new(user_params)
-   @user.save
+    @user = User.new(user_params)
+    @user.save
   end
-
 
   private
 
   def user_params
     params.require(:user).permit(:name, :last_name, :email, :phone_number)
   end
+end
