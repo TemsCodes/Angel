86431ea393e644e97235ed3605c04ff7fbcb1067
Renee Godyn <renitatje@hotmail.com>
2022-03-01 21:28:57 +0000

    if current users go to places, press on button for home bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :check_user, only: [:home]
+
+  def check_user
+    redirect_to places_path if current_user
+  end
 
   def home
   end
