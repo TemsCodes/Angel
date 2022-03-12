fb83cd83cdef8a10a473048d7c57584aef78b8cf
Tania Papamikrou <tania.papamikrou@gmail.com>
2022-03-12 12:11:30 +0000

    fixed map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
-
+import { initMapbox } from "../plugins/init_mapbox"
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 // import { review } from '../components/review';
@@ -28,6 +28,6 @@ import { displayAlertOnButtonClick } from '../components/checkin';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
 // review();
-
+initMapbox();
 displayAlertOnButtonClick();
 });
